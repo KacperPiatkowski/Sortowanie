16c3fa4a00a287b895fb8bea5973dbfc720df40b
Palczyk69 <lukasz.holowacz5@gmail.com>
2023-04-14 15:07:27 +0200

    commit 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,8 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var brute: TextView
+    private lateinit var KMP: TextView
+    private lateinit var BM: TextView
+    private lateinit var RK: TextView
+    private lateinit var wykonaj: Button
+    private lateinit var ilerazy: EditText
+    private lateinit var ileelementow: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
